2bf109f01ea90996b18c17d5471dad0db459feda
DadaYaArtem <artem.morozov2004ma@gmail.com>
2023-03-15 12:52:34 +0200

    added h2 test profile

---
+++ src/main/resources/db/migration/h2/V1__create_table_users_notes.sql
@@ -0,0 +1,20 @@
+CREATE TABLE users
+(
+    id UUID DEFAULT random_uuid(),
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    is_active boolean NOT NULL,
+    activation_code VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE notes
+(
+    id UUID DEFAULT random_uuid(),
+    name_notes VARCHAR(100) NOT NULL UNIQUE,
+    content VARCHAR(10000) NOT NULL,
+    visibility VARCHAR(100) NOT NULL,
+    user_id UUID,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_users FOREIGN KEY (user_id) REFERENCES users (id)
+);
